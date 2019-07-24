a3057eef62404db27819d34109cb5e8ef0222b66
minorigox <igor.minoru@outlook.com>
2019-07-24 11:51:24 -0300

    Criação das classes de service e resource para Cliente e correção de serialização cíclica do Json

---
+++ src/main/java/com/igorminoru/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.igorminoru.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.igorminoru.cursomc.domain.Cliente;
+import com.igorminoru.cursomc.repositories.ClienteRepository;
+import com.igorminoru.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Cliente não encontrado! Id: " + id
+					+ ", Tipo: " + Cliente.class.getName()); 
+		}
+		return obj;
+	}
+
+}
