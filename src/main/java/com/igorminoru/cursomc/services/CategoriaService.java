510d223a2bc3167b9e7323f388fce87aaca42a01
minorigox <igor.minoru@outlook.com>
2019-07-29 11:40:25 -0300

    Validação sintática com bean validation

--- src/main/java/com/igorminoru/cursomc/services/CategoriaService.java
+++ src/main/java/com/igorminoru/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.igorminoru.cursomc.domain.Categoria;
+import com.igorminoru.cursomc.dto.CategoriaDTO;
 import com.igorminoru.cursomc.repositories.CategoriaRepository;
 import com.igorminoru.cursomc.services.exception.DataIntegrityException;
 import com.igorminoru.cursomc.services.exception.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 	public Page < Categoria > findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
