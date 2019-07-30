e942ba91834019166152a14700735967dd532d65
minorigox <igor.minoru@outlook.com>
2019-07-30 12:45:25 -0300

    Correções: update das Categorias e construtor de Pagamento

--- src/main/java/com/igorminoru/cursomc/services/CategoriaService.java
+++ src/main/java/com/igorminoru/cursomc/services/CategoriaService.java
@@ -34,10 +34,11 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-
+	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void deleteById(Integer id) {
@@ -61,6 +62,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 
 }
