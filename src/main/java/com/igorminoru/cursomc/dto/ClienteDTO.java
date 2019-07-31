57176c169ce95d884bbcf2b6b6418c0619f3c778
minorigox <igor.minoru@outlook.com>
2019-07-31 14:42:31 -0300

    Validação customizada de e-mail na atualização do Cliente

--- src/main/java/com/igorminoru/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/igorminoru/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.igorminoru.cursomc.domain.Cliente;
+import com.igorminoru.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
